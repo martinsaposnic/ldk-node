017a535c9de55a3afe00d6c81146e717e8096b8b
Martin Saposnic <martinsaposnic@gmail.com>
2025-05-09 14:58:16 -0300

    wip

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let tag = "v0.5.0"
-let checksum = "fd9eb84a478402af8f790519a463b6e1bf6ab3987f5951cd8375afb9d39e7a4b"
+let tag = "v0.6.0"
+let checksum = "ec9714e6f9293a41309cb1d65d87a1bb26ad0e2aca0578abbdf8a2c860d28da2"
 let url = "https://github.com/lightningdevkit/ldk-node/releases/download/\(tag)/LDKNodeFFI.xcframework.zip"
 
 let package = Package(
